fa03e528ffce7abf48f3607c2afd2d5cfd7435c2
Oliver Brotchie <o.brotchie@gmail.com>
2021-06-27 23:07:32 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,43 +1,138 @@
-<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn4.iconfinder.com%2Fdata%2Ficons%2Fweb-design-and-development-2-11%2F66%2F80-512.png&f=1&nofb=1" alt="mosaic" title="Mosaic" width="200" height="200" />
+<div align="center">
+
+  <h1><code>block-list</code></h1>
+
+  <strong>A minimalist hosts-based tool for managing block lists and ad-blocking.</strong>
+
+</div>
 
 
-# Block List
+# About
 
 A minimalist hosts-based tool for managing block lists and ad-blocking.
 
 This project uses the excellent and regularly updated [Unified Hosts](https://github.com/StevenBlack/hosts) lists from Steven Black.
 
-### Installation
+
+
+## Why should you use Block List?
+
+- It is an extensible, small and simple tool; consisting of only fifty-five lines of code!
+- It will block this traffic across all programs running on the system. 
+- It is written in Rust and hence can be depended upon to be fast and stable. 
+- It is designed to be easily incorporated into shell scripts to automate or customize its behaviour to your liking.
+- A hosts-based block list such as this uses minimal system resources to block unwanted traffic in comparison to a browser extension.
+
+## Installation
+
+Using Cargo (Rust)
 
 ```shell
 cargo install block-list
 ```
 
-### Usage
+More options will come in future!
+
+## Usage
+
+Just use 'hosts' as the desired list if you only want ad-blocking.
 
 ```shell
 sudo block-list /path/to/hosts your-desired-list
 ```
 
-### Examples
+## Examples
 
-Basic usage:
+**Basic usage:**
 
 ```shell
-# Just use 'hosts' as the desired list if you only want ad-blocking.
 sudo block-list /etc/hosts social
 ```
 
-Pipe in a custom list:
+<br>
 
-```
+**Pipe in a custom list:**
+
+```shell
 cat someFile.txt | sudo block-list /etc/hosts
 ```
+
 **OR**
-```
+
+```shell
 cat someFile.txt | sudo block-list /etc/hosts social
 ```
-**OR**
+
+<br>
+
+**Automatically update Block List at 10pm every day:**
+
+Open the root user's crontab file
+
+```shell
+sudo crontab -e
 ```
-cat someFile.txt | sudo block-list /etc/hosts social
+
+Then add the following line
+
+```txt
+0 22 * * * block-list /etc/hosts hosts
+```
+
+<br>
+
+**Use Block List to re-route a url to a different address:**
+
+Find the address using dig
+
+```shell
+dig duckduckgo.com
+```
+
+Coppy the IP address listed in 'Answer Section' to a file
+
+```txt
+# A custom Block List
+
+# Re-route all requests from Google to DuckDuckGo
+52.142.124.215 www.google.com
+52.142.124.215 google.com
+```
+
+Pipe in the custom list to Block List
+
+```shell
+cat someFile.txt | sudo block-list /etc/hosts
+```
+
+
+## Output
+
+If successful, this command will append a new list or replace the current list at the tail of the supplied hosts file, eg:
+
+```shell
+Block List updated! 🔒
+```
+
+```txt
+##
+# Host Database
+##
+127.0.0.1	localhost
+255.255.255.255	broadcasthost
+::1             localhost
+
+
+# Your list of addresses
+1.2.3.4 somehost
+
+... 
+
+# Block List
+
+0.0.0.0 some.custom.address.net
+0.0.0.0 another.custom.address.xyz
+
+0.0.0.0 some.malware.address.com
+0.0.0.0 another.blocked.address.xxx
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,65 @@
+use std::fs;
 use std::env;
-use std::io::{self, Read};
-use std::io::BufReader;
+use std::path::Path;
+use std::io::{self};
 use std::io::prelude::*;
-use std::fs::OpenOptions;
-use std::fs::File;
 use curl::easy::{Easy2, Handler, WriteError};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args:Vec<String> = env::args().collect();
-    let mut hosts = String::new();
-    let mut custom_hosts = String::new();
 
-    // Get piped input
-    if atty::isnt(atty::Stream::Stdin) {
-        custom_hosts = io::stdin().lock().lines().map(|e| format!("\n{}",e.expect("Error with standard input."))).collect()
-    }
+    // Test for incorrect usage or help
+    if args.len() < 2 || &args[1] == "-h" || !Path::new(&args[1]).exists(){
+        if args.len() < 2 || !(&args[1] == "-h") { println!("Error! Please specify a valid path to your desired hosts file.\n") }
+        println!("USAGE:\n\tblock-list /path/to/hosts/file your-desired-block-list");
+    } else {
+        let mut hosts = String::new();
+        let mut custom_hosts = String::new();
 
-    if args.len() == 3 {
-        // Get the hosts file
-        struct Collector(Vec<u8>);
-        impl Handler for Collector {
-            fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
-                self.0.extend_from_slice(data);
-                Ok(data.len())
+        // Get piped input
+        if atty::isnt(atty::Stream::Stdin) {
+            custom_hosts = io::stdin().lock().lines().map(|e| format!("\n{}",e.expect("Error with standard input."))).collect()
+        }
+
+        if args.len() == 3 {
+            // Get the new hosts file
+            struct Collector(Vec<u8>);
+            impl Handler for Collector {
+                fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
+                    self.0.extend_from_slice(data);
+                    Ok(data.len())
+                }
+            }
+
+            let mut curl = Easy2::new(Collector(Vec::new()));
+            curl.get(true)?;
+            curl.url(&       
+                if args[2] == "hosts" {
+                    "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts".to_string()
+                } else {
+                    format!("https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/{}/hosts",args[2])
+                }[..]
+            )?;
+            curl.perform()?;
+
+            if curl.response_code()? == 200 {
+                hosts = format!("{}",String::from_utf8_lossy(&curl.get_ref().0));
+                hosts.replace_range(..hosts.match_indices("# End of custom host records.").next().unwrap().0 + 29, "");
+            } else {
+                println!("Http error: {}. See 'https://github.com/StevenBlack/hosts' for options; usage: some-list", curl.response_code()?);
             }
         }
-
-        let mut curl = Easy2::new(Collector(Vec::new()));
-        curl.get(true)?;
-        curl.url(&       
-            if args[2] == "hosts" {
-                "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts".to_string()
-            } else {
-                format!("https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/{}/hosts",args[2])
-            }[..]
-        )?;
-        curl.perform()?;
-
-        if curl.response_code()? == 200 {
-            hosts = format!("{}",String::from_utf8_lossy(&curl.get_ref().0));
-            hosts.replace_range(..hosts.match_indices("# End of custom host records.").next().unwrap().0 + 29, "");
-        } else {
-            println!("Http error: {}. See 'https://github.com/StevenBlack/hosts' for options; usage: some-list", curl.response_code()?);
+        
+        // Read current hosts file to string
+        let mut contents = fs::read_to_string(&args[1])?;
+        
+        // Remove current block list if it exists
+        if contents.contains("# Block List") {
+            contents.replace_range(contents.match_indices("# Block List").next().unwrap().0.., "")
         }
-    }
-    
-    // Open file
-    let file = OpenOptions::new()
-        .read(true)
-        .open(&args[1])?;
-    
-    // Read contents
-    let mut buf_reader = BufReader::new(&file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents)?;
 
-    // Remove current block list if it exists
-    if contents.contains("# Block List") {
-       contents.replace_range(contents.match_indices("# Block List").next().unwrap().0.., "")
+        fs::write(&args[1], format!("{}# Block List\n{}\n{}",contents,custom_hosts,hosts))?;
+        println!("Block List updated! 🔒")
     }
-
-    // Replace file with new contents
-    let mut file = File::create(&args[1])?;
-    file.write_all(format!("{}# Block List\n{}\n{}",contents,custom_hosts,hosts).as_bytes())?;
     Ok(())
 }
